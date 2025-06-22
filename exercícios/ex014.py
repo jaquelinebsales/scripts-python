59a0b1de376b9ea107d440a7bdacaf8f1d8e2ed0
Ana Jaqueline <jaquelinesales200729@gmail.com>
2025-06-22 20:20:24 -0300

    ex 14 e 15

---
+++ exercícios/ex014.py
@@ -0,0 +1,2 @@
+tempC = float(input("Digite a temperatura em graus Celsius: "))
+print(f"A temperatura em Farenhenit é: {((tempC*9)/5)+32}")
---
+++ exercícios/ex015.py
@@ -0,0 +1,3 @@
+dias = int(input("Por quantos dias esse carro foi alugado? "))
+km = int(input("Quantos km foi andado? "))
+print(f"Total a pagar: R${dias*60+km*0.15}")
